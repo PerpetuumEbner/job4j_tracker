cf3f76412fcbe64aa1a507871fabb0f02f277c93
Stanislav <yustas@protonmail.com>
2021-10-12 11:55:09 +0500

    2.5.2.1. Тест на ValidateInput.

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -14,7 +14,7 @@ public class ValidateInputTest {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         PrintStream printStream = System.out;
         System.setOut(new PrintStream(byteArrayOutputStream));
-        ValidateInput input = new ValidateInput(new StubInput(new String[] {"one", "1"}));
+        ValidateInput input = new ValidateInput(new StubInput(new String[]{"one", "1"}));
         input.askInt("Enter");
         assertThat(
                 byteArrayOutputStream.toString(),
@@ -28,12 +28,45 @@ public class ValidateInputTest {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         PrintStream printStream = System.out;
         System.setOut(new PrintStream(byteArrayOutputStream));
-        ValidateInput input = new ValidateInput(new StubInput(new String[] {"5", "1"}));
+        ValidateInput input = new ValidateInput(new StubInput(new String[]{"5", "1"}));
         input.askInt("Enter", 5);
         assertThat(
                 byteArrayOutputStream.toString(),
                 is(String.format("Please select key from menu.%n"))
         );
+        System.setOut(printStream);
+    }
+
+    @Test
+    public void whenTheValidInput() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = System.out;
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        ValidateInput input = new ValidateInput(new StubInput(new String[]{"1"}));
+        int selected = input.askInt("Enter");
+        assertThat(selected, is((1)));
+        System.setOut(printStream);
+    }
+
+    @Test
+    public void whenSeveralRightInput() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = System.out;
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        ValidateInput input = new ValidateInput(new StubInput(new String[]{"1", "2", "3"}));
+        int selected = input.askInt("Enter");
+        assertThat(selected, is((1)));
+        System.setOut(printStream);
+    }
+
+    @Test
+    public void whenNegativeNumberInput() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = System.out;
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        ValidateInput input = new ValidateInput(new StubInput(new String[]{"-1"}));
+        int selected = input.askInt("Enter");
+        assertThat(selected, is((-1)));
         System.setOut(printStream);
     }
 }
