69518968c390a94817d207a15a5a845c98f7b6ed
Stanislav <yustas@protonmail.com>
2022-07-20 11:38:23 +0500

    Исправлены настройки подключения к базе даных H2.

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,7 +1,6 @@
 create table if not exists items
 (
-    id          serial primary key,
-    name        text,
-    description text,
-    created     timestamp
+    id      serial primary key,
+    name    text,
+    created timestamp
 );
