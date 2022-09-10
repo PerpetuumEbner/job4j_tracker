afdeac0fc8322d0c3846223883547e0bdf13d49e
Stanislav <yustas@protonmail.com>
2022-09-10 14:21:51 +0500

    3.1.1. Lombok. Настройка IDEA.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,13 +1,17 @@
 package ru.job4j.tracker;
 
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode (of = "id")
 @Entity
 @Table(name = "items")
 public class Item implements Comparable<Item> {
@@ -26,9 +30,6 @@ public class Item implements Comparable<Item> {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
-    public Item() {
-    }
-
     public Item(String name) {
         this.name = name;
     }
@@ -44,46 +45,6 @@ public class Item implements Comparable<Item> {
         this.time = time;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Timestamp getTime() {
-        return time;
-    }
-
-    public void setTime(Timestamp timestamp) {
-        this.time = timestamp;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
     @Override
     public String toString() {
         return created.format(FORMATTER) + System.lineSeparator() + id + " " + name;
@@ -92,22 +53,5 @@ public class Item implements Comparable<Item> {
     @Override
     public int compareTo(Item anotherItem) {
         return name.compareTo(anotherItem.getName());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return Objects.equals(id, item.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
